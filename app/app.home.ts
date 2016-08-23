969d3c7418309aeca8da75c27215eec3b5fd9c8f
javalv <java.com.lv@163.com>
2016-08-23 18:12:25 +0800

    remove bin folder all file out of control

--- app/app.home.ts
+++
@@ -1,7 +0,0 @@
-import {Component} from '@angular/core';
-@Component({
-    selector: 'my-home-page',
-    template: `HOME`
-})
-export class MyHomePageComponent {
-}
--- app/app.routing.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var router_1 = require('@angular/router');
-var app_home_1 = require("./app.home");
-var appRoutes = [
-    { path: 'home', component: app_home_1.MyHomePageComponent },
-];
-exports.appRoutingProviders = [];
-exports.routing = router_1.RouterModule.forRoot(appRoutes);
-//# sourceMappingURL=app.routing.js.map
