3dcc5f05745a7e8489b3112cfd2851b7eb720500
lin <java.com.lv@163.com>
2016-08-28 20:40:58 +0800

    更新

--- app/serviceSubjectNotice/MissionService.ts
+++ app/serviceSubjectNotice/MissionService.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Subject }    from 'rxjs/Subject';
+import { Subject,Observable }    from 'rxjs/Subject';
+
 @Injectable()
 export class MissionService {
     // Observable string sources
     private missionAnnouncedSource = new Subject<string>();
     private missionConfirmedSource = new Subject<string>();
     // Observable string streams
-    missionAnnounced$ = this.missionAnnouncedSource.asObservable();
-    missionConfirmed$ = this.missionConfirmedSource.asObservable();
+    missionAnnounced$:Observable = this.missionAnnouncedSource.asObservable();
+    missionConfirmed$:Observable = this.missionConfirmedSource.asObservable();
     // Service message commands
     announceMission(mission: string) {
         this.missionAnnouncedSource.next(mission);
