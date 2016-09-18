c6d7705cdc6eeb047ca253becb240ac36e807423
lin <java.com.lv@163.com>
2016-09-18 23:47:41 +0800

    更新

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,12 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: `<h1>Component Router</h1>
-                <nav>
-                    <a routerLink="/home" routerLinkActive="active">Crisis Center</a>
-                    <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
-                    <a routerLink="/subject" routerLinkActive="active">subject</a>
-                    <a routerLink="/output" routerLinkActive="active">output</a>
-                    <a routerLink="/es6" routerLinkActive="active">es6</a>
-                </nav>
-                <router-outlet></router-outlet>`
+    templateUrl: 'app.component.html'
 })
 export class AppComponent {
+    showHeading = true;
+    heroes = ['Magneta', 'Bombasto', 'Magma', 'Tornado'];
+    toggleHeading() {
+        this.showHeading = !this.showHeading;
+    }
 }
